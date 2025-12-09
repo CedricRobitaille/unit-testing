181955ca770d20bfe53832d0191d06253bf677b3
Cedric <crubbertire@rogers.com>
2025-12-09 18:12:04 -0500

    Created testing for all routes

--- back-end/server.test.js
+++ back-end/server.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const {app, server} = require("./server")
 
 describe("Test the root path", () => {
-  test("It sshould respond with 'Hello World!'", async () => {
+  test("It should respond with 'Hello World!'", async () => {
     const response = await request(app).get("/");
     expect(response.text).toBe("Hello World!")
     expect(response.statusCode).toBe(200);
@@ -11,13 +11,30 @@ describe("Test the root path", () => {
 
 
 
-describe("Test the users endpoints", () => {
-  test("It should create a new user", async () => {
+describe("Test the todo endpoints", () => {
+  test("It should create a new todo", async () => {
     const response = await request(app)
       .post('/todos')
       .send({ todo: "Example Todo" })
     expect(response.body).toEqual({ todo: "Example Todo" })
+    expect(response.statusCode).toBe(200);
   })
+
+  test("It should update a todo", async () => {
+    const response = await request(app)
+      .put('/todos/1')
+      .send({ todo: "Updated Todo" })
+    expect(response.body).toEqual({ id: "1", todo: "Updated Todo" })
+    expect(response.statusCode).toBe(200);
+  })
+
+  test("It should delete a todo", async () => {
+    const response = await request(app).delete('/todos/1')
+    expect(response.body).toEqual({ id: "1" })
+    expect(response.statusCode).toBe(200)
+  })
+
+
 })
 
 afterAll(done => {
